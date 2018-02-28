9eadcf29e3b214501aef9199bb3f09d7a2d07fb4
rkaune <rkaune@ugcloud.ca>
2018-02-28 13:28:32 -0500

    I Upper cased the WORLD!

--- src/helloworld7/HelloWorld7.java
+++ src/helloworld7/HelloWorld7.java
@@ -16,7 +16,7 @@ public class HelloWorld7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!!!!!!");
+        System.out.println("Hello WORLD!!!!!!");
     }
     
 }
